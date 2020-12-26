6820fce1dcd59554aac50b15d59cd94a7646b209
Your Name <you@example.com>
2020-12-26 16:18:55 +0530

    first commit

--- kidzgalaxy/thought/views.py
+++ kidzgalaxy/thought/views.py
@@ -1,5 +1,9 @@
+from django.core import serializers
+from django.http import JsonResponse, HttpResponse
+
 from django.shortcuts import render
 from . import urls
+import json
 
 from datetime import datetime
 from time import gmtime, strftime
@@ -10,6 +14,14 @@ from .models import Thought
 def index(request):
     thought=Thought.objects.all()
     showtime = strftime("%d-%b", gmtime())
+
+    post_list = serializers.serialize('json', thought)
+    # print(post_list)
+    # return JsonResponse(post_list,safe=False)
+    #return JsonResponse(list(Thought.objects.all().values()), safe=False)
+
+    #return JsonResponse(json.dumps({'thought':thought,'showtime':showtime}))
+    # return HttpResponse({'thought':thought,'showtime':showtime}, mimetype="application/json")
     return render(request,'index.html',{'thought':thought,'showtime':showtime})
 
 
